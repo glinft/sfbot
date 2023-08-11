c75b2f6edc9d08bd2b5dc47639d68cca4c6fc3cf
Glenn Lin <xmglin@gmail.com>
2023-08-11 03:08:48 +0000

    Feat: Slack +install +oauthredirect

--- channel/slack/slack_channel.py
+++ channel/slack/slack_channel.py
@@ -2,7 +2,8 @@
 
 import re
 import json
-from flask import Flask, request
+import html
+from flask import Flask, request, make_response
 from flask_cors import CORS
 from common import const
 from common import log
@@ -11,6 +12,20 @@ from config import common_conf_val, channel_conf
 from channel.channel import Channel
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_sdk.oauth import AuthorizeUrlGenerator
+from slack_sdk.oauth.installation_store import FileInstallationStore, Installation
+from slack_sdk.oauth.state_store import FileOAuthStateStore
+from slack_sdk.web import WebClient
+
+client_id = channel_conf(const.SLACK).get('slack_client_id')
+client_secret = channel_conf(const.SLACK).get('slack_client_secret')
+state_store = FileOAuthStateStore(expiration_seconds=600, base_dir="/opt/boltstore/states")
+installation_store = FileInstallationStore(base_dir="/opt/boltstore/installations")
+authorize_url_generator = AuthorizeUrlGenerator(
+    client_id=client_id,
+    scopes=["app_mentions:read", "channels:history", "channels:read", "chat:write", "commands", "files:write", "groups:history", "groups:read", "im:history", "im:read", "mpim:history", "mpim:read", "team.billing:read", "team:read", "users:read"],
+    user_scopes=[],
+)
 
 app = App(
     token=channel_conf(const.SLACK).get('slack_bot_token'),
@@ -37,14 +52,14 @@ def handle_home_opened(event):
 @app.event("app_mention")
 def handle_mention(event, say):
     event_type = event["type"]
-    msg_id = event["client_msg_id"]
+    msg_id = event.get("client_msg_id",'n/a')
     log.info(f"## Slack Event: {event_type}/{msg_id}")
     pass
 
 @app.event("message")
 def handle_message(event, say):
-    msg_id = event["client_msg_id"]
-    event_type = event["type"] # message
+    event_type = event["type"]
+    msg_id = event.get("client_msg_id",'n/a')
     log.info(f"## Slack Message: {event_type}/{msg_id}")
     event_ts = event["event_ts"]
     ts = event["ts"]
@@ -52,7 +67,7 @@ def handle_message(event, say):
         ts = event["thread_ts"]
     channel_type = event["channel_type"] # im/channel
     channel = event["channel"]
-    team = event["team"]
+    team = event.get("team",'n/a')
     user = event["user"]
     text = event["text"]
     if channel_type=="im":
@@ -113,6 +128,71 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     return response
 
+@flask_app.route("/slack/install", methods=["GET"])
+def slack_install():
+    state = state_store.issue()
+    url = authorize_url_generator.generate(state)
+    return f'<a href="{html.escape(url)}">' \
+           f'<img alt=""Add to Slack"" height="40" width="139" ' \
+           f'src="https://platform.slack-edge.com/img/add_to_slack.png" ' \
+           f'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, ' \
+           f'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
+
+@flask_app.route("/slack/oauth_redirect", methods=["GET"])
+def slack_oauth_redirect():
+    if "code" in request.args:
+        code=request.args["code"]
+        if state_store.consume(request.args["state"]):
+            log.info(f"== client:{client_id} code:{code}")
+            client = WebClient()
+            oauth_response = client.oauth_v2_access(
+                client_id=client_id,
+                client_secret=client_secret,
+                # redirect_uri=redirect_uri,
+                code=code
+            )
+            # log.info(oauth_response)
+            installed_enterprise = oauth_response.get("enterprise") or {}
+            is_enterprise_install = oauth_response.get("is_enterprise_install")
+            installed_team = oauth_response.get("team") or {}
+            installer = oauth_response.get("authed_user") or {}
+            incoming_webhook = oauth_response.get("incoming_webhook") or {}
+            bot_token = oauth_response.get("access_token")
+            bot_id = None
+            enterprise_url = None
+            if bot_token is not None:
+                auth_test = client.auth_test(token=bot_token)
+                bot_id = auth_test["bot_id"]
+                if is_enterprise_install is True:
+                    enterprise_url = auth_test.get("url")
+            installation = Installation(
+                app_id=oauth_response.get("app_id"),
+                enterprise_id=installed_enterprise.get("id"),
+                enterprise_name=installed_enterprise.get("name"),
+                enterprise_url=enterprise_url,
+                team_id=installed_team.get("id"),
+                team_name=installed_team.get("name"),
+                bot_token=bot_token,
+                bot_id=bot_id,
+                bot_user_id=oauth_response.get("bot_user_id"),
+                bot_scopes=oauth_response.get("scope"),
+                user_id=installer.get("id"),
+                user_token=installer.get("access_token"),
+                user_scopes=installer.get("scope"),
+                incoming_webhook_url=incoming_webhook.get("url"),
+                incoming_webhook_channel=incoming_webhook.get("channel"),
+                incoming_webhook_channel_id=incoming_webhook.get("channel_id"),
+                incoming_webhook_configuration_url=incoming_webhook.get("configuration_url"),
+                is_enterprise_install=is_enterprise_install,
+                token_type=oauth_response.get("token_type"),
+            )
+            installation_store.save(installation)
+            return "Thanks for installing this app!"
+        else:
+            return make_response(f"Try the installation again (the state value is already expired)", 400)
+
+    error = request.args["error"] if "error" in request.args else ""
+    return make_response(f"Something is wrong with the installation (error: {html.escape(error)})", 400)
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
