fd2f548152fae2ad68bbf66482d402871c8ec620
Glenn Lin <xmglin@gmail.com>
2023-11-23 07:42:04 +0000

    minor fix

--- model/openai/chatgpt_model.py
+++ model/openai/chatgpt_model.py
@@ -573,13 +573,10 @@ class Session(object):
             system_prompt = 'You are answering the question just like you are the owner or partner of the company described in the context.'
             if isinstance(character_desc, str) and character_desc != 'undef' and len(character_desc) > 0:
                 system_prompt = character_desc
-            if isinstance(character_id, str) and character_id.startswith('x'):
-                system_prompt += f"\n{config_prompt}\n```"
-            else:
-                system_prompt += '\nIf you don\'t know the answer, just say you don\'t know. DO NOT try to make up an answer.'
-                system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
-                system_prompt += '\nIf you are unclear about the question, politely respond that you need a clearer and more detailed description.'
-                system_prompt += f"\n{config_prompt}\n```"
+            system_prompt += '\nIf you don\'t know the answer, just say you don\'t know. DO NOT try to make up an answer.'
+            system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
+            system_prompt += '\nIf you are unclear about the question, politely respond that you need a clearer and more detailed description.'
+            system_prompt += f"\n{config_prompt}\n```"
             for doc, score in docs:
                 log.info("[FAISS] {} {}".format(score, json.dumps(doc.metadata)))
                 '''
@@ -656,9 +653,12 @@ class Session(object):
             session.append(user_item)
             return session, [], [], similarity, False
 
-        system_prompt += '\nIf you don\'t know the answer, just say you don\'t know. DO NOT try to make up an answer.'
-        system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
-        system_prompt += '\nIf you are unclear about the question, politely respond that you need a clearer and more detailed description.'
+        if isinstance(character_id, str) and character_id.startswith('x'):
+            log.info("[CHATGPT] teambot character id={} add context".format(character_id))
+        else:
+            system_prompt += '\nIf you don\'t know the answer, just say you don\'t know. DO NOT try to make up an answer.'
+            system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
+            system_prompt += '\nIf you are unclear about the question, politely respond that you need a clearer and more detailed description.'
 
         if len(docs) == 0 and qna_output is None:
             log.info("[CHATGPT] prompt(nodoc)={}".format(system_prompt))
@@ -671,8 +671,7 @@ class Session(object):
             session.append(user_item)
             return session, [], [], similarity, False
 
-        system_prompt += '\n' + config_prompt
-        system_prompt += '\nContext:\n```'
+        system_prompt += f"\n{config_prompt}\n```"
         if qna_output is not None:
             system_prompt += '\n' + qna_output
         for i, doc in enumerate(docs):
