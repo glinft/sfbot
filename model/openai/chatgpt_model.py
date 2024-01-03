ef163fb4e6af90b1e24975fa4585861c0bdf7d39
Glenn Lin <xmglin@gmail.com>
2024-01-03 02:06:08 +0000

    Fix: remove (tuned to only answer) instruction

--- model/openai/chatgpt_model.py
+++ model/openai/chatgpt_model.py
@@ -714,7 +714,7 @@ class Session(object):
             log.info("[CHATGPT] teambot character id={} add context".format(character_id))
         else:
             system_prompt += '\nIf you don\'t know the answer, just say you don\'t know. DO NOT try to make up an answer.'
-            system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
+            # system_prompt += '\nIf the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.'
             system_prompt += '\nIf you are unclear about the question, politely respond that you need a clearer and more detailed description.'
 
         if len(docs) == 0 and qna_output is None:
